f92f6bfa678d8ede25738550c9fa4fabe69470ca
Stefan Gouldson <stefan@Stefans-MacBook-Pro-Cool-Studio.local>
2021-08-09 15:44:44 +0100

    pages added

---
+++ pages/news/[newsID].js
@@ -0,0 +1,10 @@
+
+const NewsDetails = () => {
+  return (
+    <div>
+      <h1>News Detail</h1>
+    </div>
+  )
+}
+
+export default NewsDetails
---
+++ pages/news/index.js
@@ -0,0 +1,9 @@
+const news = () => {
+  return (
+    <div>
+      <h1>News Page</h1>
+    </div>
+  )
+}
+
+export default news
