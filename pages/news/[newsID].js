1d4a43533703638b132a86dccd46cd13361a73a6
Stefan Gouldson <stefan@Stefans-MacBook-Pro-Cool-Studio.local>
2021-08-09 15:47:37 +0100

    added page queries

--- pages/news/[newsID].js
+++ pages/news/[newsID].js
@@ -1,8 +1,15 @@
+import { useRouter } from "next/dist/client/router"
 
 const NewsDetails = () => {
+  const router = useRouter();
+
+  const newsID = router.query.newsID
+
+  // Send request to find news
+
   return (
     <div>
-      <h1>News Detail</h1>
+      <h1>News Detail: {newsID}</h1>
     </div>
   )
 }
