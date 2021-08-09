b462b782a15e8151313033f353e9fc1e35cc4fdc
Stefan Gouldson <stefan@Stefans-MacBook-Pro-Cool-Studio.local>
2021-08-09 15:53:10 +0100

    links added

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,8 +1,14 @@
+import Link from 'next/link'
+
 const news = () => {
   return (
-    <div>
-      <h1>News Page</h1>
-    </div>
+    <>
+    <h1>News Page</h1>
+    <ul>
+      <li><Link href="/news/nextjs-is-great">Next JS is good</Link></li>
+      <li><Link href="/news/something-else">Something else</Link></li>
+    </ul>
+    </>
   )
 }
 
